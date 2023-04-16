0ef611512c702f874eb334222402c1ef16203e6a
juripfammatter <119895438+juripfammatter@users.noreply.github.com>
2023-04-16 15:37:33 +0200

    added comments

--- rbd_ros/rbd_pos_controller/include/rbd_pos_controller/RbdPosController.hpp
+++ rbd_ros/rbd_pos_controller/include/rbd_pos_controller/RbdPosController.hpp
@@ -13,37 +13,60 @@
 
 namespace rbd_pos_controller {
 
-/*!
+/**
  * Main class for the node to handle the ROS interfacing.
  */
 class RbdPosController
 {
  public:
-  /*!
+  /**
    * Constructor.
    * @param nodeHandle the ROS node handle.
    */
   RbdPosController(ros::NodeHandle& nodeHandle);
 
-  /*!
+  /**
    * Destructor.
    */
   virtual ~RbdPosController();
 
 
  private:
-  /*!
+  /**
    * Reads and verifies the ROS parameters.
    * @return true if successful.
    */
   bool readParameters();
 
+  /**
+   * @brief function to saturate/ cut off controller output
+   * 
+   * @param value 
+   * @param lower_limit 
+   * @param upper_limit 
+   * @return saturated value
+   */
   float saturate(double value, double lower_limit, double upper_limit);
-  void updateGain(void);
+  
+  //void updateGain(void);
+
+  /**
+   * @brief envelope function to minimize large side forces
+   * 
+   * @param vx x velocity
+   * @param vy y velocity
+   */
   void envelope(double &vx, double &vy);
+
+  /**
+   * @brief conversion from geometry_msgs::Pose to std::vector<double> RPY
+   * 
+   * @param pose geometry_msgs::Pose
+   * @param rpy std::vector<double> RPY
+   */
   void quat2eul(const geometry_msgs::Pose& pose,  std::vector<double> &rpy);
 
-  /*!
+  /**
    * ROS topic callback method.
    * @param message the received message.
    */
@@ -56,13 +79,19 @@ class RbdPosController
    * @return true if successful, false otherwise.
    */
   bool emStopCallback(std_srvs::SetBool::Request& request,
-                       std_srvs::SetBool::Response& response);
+                      std_srvs::SetBool::Response& response);
 
+   /**
+   * ROS service server callback.
+   * @param request the request of the service.
+   * @param response the provided response.
+   * @return true if successful, false otherwise.
+   */
   bool setPosCallback(rbd_msgs::SetPosition::Request& request,
-                       rbd_msgs::SetPosition::Response& response);
+                      rbd_msgs::SetPosition::Response& response);
 
                     
-  //
+  /* ROS definitions */
   ros::NodeHandle& nodeHandle_;
   ros::Subscriber subscriber_;
   ros::Publisher cmdVelPublisher_;
@@ -70,22 +99,25 @@ class RbdPosController
   ros::ServiceServer emServiceServer_;
   ros::ServiceServer posServiceServer_;
   ros::ServiceClient poseClient_;
-  qre_msgs::SetBodyPose pose_srv;
 
+  /* Topic and Service declarations */
+  qre_msgs::SetBodyPose pose_srv;
   std::string sub_topic;
   std::string cmd_vel_pub_topic;
   std::string status_pub_topic;
   std::string body_pose_service;
   std::string emergency_stop_service;
   std::string set_position_service;
+
+  /* Topic and Service message declarations */
   std_msgs::String status_message;
   geometry_msgs::Twist stop_message;
   geometry_msgs::Twist vel_message;
   geometry_msgs::Twist zero_twist;
   geometry_msgs::Point goal_position;
 
+  /* Global variables */
   double goal_roll = 0, goal_pitch = 0, goal_yaw = 0;
-  double remaining_distance;
 
   bool emergency_stop = true;
   
@@ -93,10 +125,12 @@ class RbdPosController
   double kp_angular;
   double kp_angular_lin;
   double kp_linear;
-  double k_roll = 2.865, k_pitch = 2.865, k_yaw=2.046;
-  double zero_height = 0.41;
-  double ctrl_vel_x, ctrl_vel_y;
 
+  double k_roll = 2.865, k_pitch = 2.865, k_yaw=2.046;
+  //double zero_height = 0.41;
+  //double ctrl_vel_x, ctrl_vel_y;
+
+  /* Control state for state machine */
   typedef enum{
       PRE_ROTATION,
       LIN_MOVEMENT,
