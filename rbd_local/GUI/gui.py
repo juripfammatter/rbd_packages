a09952ef006353df8de1cb9c6403706b43a6678b
juripfammatter <119895438+juripfammatter@users.noreply.github.com>
2023-04-25 18:25:13 +0200

    added minimal functionality

--- rbd_local/GUI/gui.py
+++ rbd_local/GUI/gui.py
@@ -2,82 +2,120 @@
 """
 Created on Mon Apr 24 18:09:22 2023
 
-@author: jurip
+@author: juri pfammatter
 """
 
 
 import customtkinter
 import subprocess
 
-customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-customtkinter.set_widget_scaling(0.1)  # window geometry dimensions
-customtkinter.set_window_scaling(0.2)  # window geometry dimensions
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-title_font = ("Ubuntu Mono", 87)
-text_font = ("Ubuntu Mono", 50)
+        self.em_stop = True
 
+        customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+        customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+        customtkinter.set_widget_scaling(0.1)  # window geometry dimensions
+        customtkinter.set_window_scaling(0.2)  # window geometry dimensions
 
-app = customtkinter.CTk(fg_color="#101020")
-app.geometry("3000x2000")
-app.title("CustomTkinter simple_example.py")
+        title_font = ("Ubuntu Mono", 87)
+        text_font = ("Ubuntu Mono", 50)
 
-def button_callback():
-    print("Button click")
+        self.geometry("1700x2000")
+        self.title("RoboDog III User Interface")
 
+        # Main Frame
+        master_frame = customtkinter.CTkFrame(master=self, fg_color="#101020")
+        master_frame.pack(pady=0, padx=0, fill="both", expand=True)
 
-# def slider_callback(value):
-#     progressbar_1.set(value)
-    
-def run_script(self):
-    # Replace "path/to/script.sh" with the actual path to your Bash script
-    subprocess.call(["/bin/bash", "path/to/script.sh"])
-
-
-frame_1 = customtkinter.CTkFrame(master=app, fg_color="#202030")
-frame_1.pack(pady=70, padx=20, fill="both", expand=True)
-
-# label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT, font = gui_font, text="RoboDog III")
-# label_1.pack(pady=40, padx=30)
+        sub_master_frame = customtkinter.CTkFrame(master=master_frame, fg_color="#202030")
+        sub_master_frame.pack(pady=50, padx=20, fill="both", expand=True)
 
 
-title_frame = customtkinter.CTkFrame(master=frame_1, fg_color="#202030")
-title_frame.pack(pady=20, padx=20)
+        # Title Frame
+        title_frame = customtkinter.CTkFrame(master=sub_master_frame, fg_color="#202030")
+        title_frame.pack(pady=20, padx=20)
 
-title = customtkinter.CTkLabel(master=title_frame, justify=customtkinter.LEFT, font = title_font, text= "RoboDog III User Interface")
-title.pack(padx=10, pady = 10)
+        title = customtkinter.CTkLabel(master=title_frame, justify=customtkinter.LEFT, font = title_font, text= "RoboDog III User Interface")
+        title.pack(padx=10, pady = 10)
 
+        # Media Frame
+        media_frame = customtkinter.CTkFrame(master=sub_master_frame, 
+                                             fg_color="#303040")
 
-media_frame = customtkinter.CTkFrame(master=frame_1, fg_color="#303040")
-media_frame.pack(pady=20, padx=20,fill="both", expand=True)
-
-
-grid_frame = customtkinter.CTkFrame(master=media_frame, fg_color="#303040")
-grid_frame.grid(row = 2, column = 2, pady=0, padx=0)
-
-label_1 = customtkinter.CTkLabel(master=grid_frame, justify=customtkinter.LEFT, font = text_font, text= "Emergency Stop")
-label_1.grid(row=0, column=0, padx=300, pady = 50)
-
-label_2 = customtkinter.CTkLabel(master=grid_frame, justify=customtkinter.CENTER, font=text_font, text = "Current State")
-label_2.grid(row=0, column=1, padx=200, pady = 50)
-
-#Add Button
-button_1 = customtkinter.CTkButton(master=grid_frame, command=button_callback, font=text_font, text= "Emergency Stop")
-button_1.grid(row=1, column=0, padx=200, pady = 10)
-
-# #Create Frame & Label
-# # master_frame = customtkinter.CTkFrame(master=app)
-# # frame_left = customtkinter.CTkFrame(master=master_frame)
-
-# # # master_frame.grid(row=0, column=0, padx=50, pady=50, sticky="nsew")
-
-# # label_1 = customtkinter.CTkLabel(master=grid_frame, justify=customtkinter.LEFT, font = gui_font)
-# # label_1.pack(padx=200, pady = 200)
+        media_frame.pack(pady=20, padx=20,fill="both", expand=True)
 
 
-# # # label_1.pack(pady=10, padx=10)
+        grid_frame = customtkinter.CTkFrame(master=media_frame, 
+                                            fg_color="#303040")
+
+        grid_frame.grid(row = 2, column = 2, pady=0, padx=100)
 
 
+        # Labels
+        label_1 = customtkinter.CTkLabel(master=grid_frame, 
+                                         justify=customtkinter.LEFT, 
+                                         font = text_font, 
+                                         text= "Emergency Stop:")
 
+
+        label_2 = customtkinter.CTkLabel(master=grid_frame, 
+                                         justify=customtkinter.CENTER, 
+                                         font=text_font, 
+                                         text = "Current State:")
+        
+        self.status_text = customtkinter.CTkLabel(master=grid_frame, 
+                                            justify=customtkinter.CENTER, 
+                                            font=text_font, 
+                                            text_color= "#FF0000",
+                                            text = "Emergency Stop")
+
+
+        # Buttons
+        self.em_button = customtkinter.CTkButton(master=grid_frame, 
+                                           command=self.em_callback, 
+                                           font=text_font,
+                                           border_spacing=30,
+                                           hover_color="#303040",
+                                           fg_color="#505080",
+                                           text= "Disable",
+                                           width=600)
+
+
+        
+
+
+        label_1.grid(row=0, column=0, padx=200, pady = 50)
+        label_2.grid(row=1, column=0, padx=200, pady = 50)
+        self.em_button.grid(row=0, column=1, padx=10, pady = 50)
+        self.status_text.grid(row=1, column=1, padx=10, pady = 50)
+
+    def em_callback(self):
+        print("Initiating emergency stop")
+        if(self.em_stop):
+            self.em_stop = False
+
+            self.status_text.configure(text = "Idle", text_color = "#FFFFFF")
+            self.em_button.configure(text = "Enable")
+
+        else:
+            self.em_stop = True
+
+            self.status_text.configure(text = "Emergency Stop", text_color = "#FF0000")
+            self.em_button.configure(text = "Disable")
+
+    def run_script(self):
+        # Replace "path/to/script.sh" with the actual path to your Bash script
+        subprocess.call(["/bin/bash", "path/to/script.sh"])
+
+    def set_state(self, state, color):
+        self.status_text.configure(text = state, text_color = color)
+        
+        
+
+
+app = App()
 app.mainloop()
 
